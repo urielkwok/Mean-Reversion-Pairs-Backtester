c2073854765a28aa6c89426d8179ceae3b941121
urielkwok <urielkwok@gmail.com>
2026-03-04 18:44:33 -0500

    Update main.py
    Uses boolean output to check if spread is stationary or not

--- main.py
+++ main.py
@@ -7,5 +7,8 @@ STOCK_2 = "PEP"
 
 stock_df = dl.get_data(STOCK_1, STOCK_2, START_DATE, END_DATE)
 spread = stock_df[STOCK_1] - stock_df[STOCK_2]
-adf_result = an.adf_test(spread)
-print(adf_result)
+stationary = an.adf_test(spread)
+if stationary is True:
+    print("Spread is stationary")
+else:
+    print("Spread is not stationary.")
