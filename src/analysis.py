860f3d3bb25075468130041de99a23592f3dade8
urielkwok <urielkwok@gmail.com>
2026-03-04 18:43:29 -0500

    Update analysis.py
    Returns a boolean instead of a string indicating if series is stationary or not using a p-value of 0.05

--- src/analysis.py
+++ src/analysis.py
@@ -2,11 +2,15 @@ import numpy as np
 from statsmodels.tsa.stattools import adfuller as adf
 
 
-def adf_test(series: np.ndarray) -> str:
+def adf_test(series: np.ndarray) -> bool:
     """
     Requires: Nothing
     Modifies: Nothing
-    Returns: Dict containing Raw ADF and p-value for the series.
+    Returns: True if series is stationary, False otherwise
     """
     result = adf(series)
-    return f"Raw ADF: {result[0]:.4f}, p-value: {result[1]:.4f}"
+    print(f"Raw ADF: {result[0]:.4f}, p-value: {result[1]:.4f}")
+    if result[1] < 0.05:
+        return True
+    else:
+        return False
